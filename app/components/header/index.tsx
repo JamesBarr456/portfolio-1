06a5fa3340ddc2455c9fdc198e0fd7222ca06c5a
Emmanuel <barretosantiago96@hotmail.com>
2025-01-21 12:06:05 -0300

    header p.2

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -1,4 +1,6 @@
 
+'use client'
+
 /**
  * 
  * Librerias u otros
@@ -6,14 +8,20 @@
 
 import Image from "next/image"
 import Link from "next/link"
+/**
+ * 
+ * Node modules
+ */
 
 /**
  * 
  * Components
  */
 import Navbar from "./navbar"
+import { useState } from "react"
 
 export const Header = () => {
+    const [navOpen, SetNavOpen] = useState(false)
   return (
    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
     <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid-cols-[1fr,3fr,1fr] ">
@@ -28,17 +36,18 @@ export const Header = () => {
         </h1>
 
         <div className="relative md:justify-self-center">
-            <button className="menu-btn md:hidden" >
+            <button className="menu-btn md:hidden"
+            onClick={() => SetNavOpen((prev) => !prev)} >
                 <span className="material-symbols-rounded ">
-                    {/* agregar icono de menu */}
+                    {navOpen ? 'close' : 'menu'}
                     menu
                 </span>
             </button>
-            <Navbar/>
+            <Navbar navOpen={navOpen}/>
         </div>
 
         <Link href={"#contact"} 
-        className="">
+        className="btn btn-secondary max-md:hidden md:justify-self-center">
             Contact Me
         </Link>
     </div>
--- app/components/header/navbar/index.tsx
+++ app/components/header/navbar/index.tsx
@@ -1,12 +1,68 @@
-import { navItems } from "@/app/data/navbar-links"
-import Link from "next/link"
 
-const Navbar = () => {
-  return (
-   <nav className="">
+
+import Link from "next/link"
+import { useEffect, useRef } from "react";
+
+interface Props {
+    navOpen: boolean
+}
+const Navbar = ({navOpen}: Props) => {
+    const lastActiveLink = useRef()
+    const activeBox = useRef()
+
+    const initActiveBox = () => {
+        activeBox.current.style.top = event.target.offsetTop + 'px'
+        activeBox.current.style.left = lastActiveLink.current.offsetLeft + 'px'
+        activeBox.current.style.width = lastActiveLink.current.offsetWidth + 'px'
+        activeBox.current.style.heigth = lastActiveLink.current.offsetHeigth + 'px'
+    }
+
+    useEffect(initActiveBox,[] )
+    window.addEventListener('resize', initActiveBox)
+    const activeCurrentLink = (event) => {
+        lastActiveLink.current?.classList.remove('active')
+        event.target.classList.add('active')
+        lastActiveLink.current = event.target
+
+        activeBox.current.style.top = event.target.offsetTop + 'px'
+        activeBox.current.style.left = event.target.offsetLeft + 'px'
+        activeBox.current.style.width = event.target.offsetWidth + 'px'
+        activeBox.current.style.heigth = event.target.offsetHeigth + 'px'
+
+    }
+    const navItems = [
     {
-        navItems.map(({className,label,link},key) => (
-            <Link key={key} href={link} className={className}>
+      label: 'Home',
+      link: '#home',
+      className: 'nav-link active',
+      ref: lastActiveLink
+    },
+    {
+      label: 'About',
+      link: '#about',
+      className: 'nav-link'
+    },
+    {
+      label: 'Work',
+      link: '#work',
+      className: 'nav-link'
+    },
+    {
+      label: 'Reviews',
+      link: '#reviews',
+      className: 'nav-link'
+    },
+    {
+      label: 'Contact',
+      link: '#contact',
+      className: 'nav-link md:hidden'
+    }
+  ];
+  return (
+   <nav className={'navbar' + (navOpen ? 'active' : '') }>
+    {
+        navItems.map(({className,label,link, ref},key) => (
+            <Link key={key} href={link} className={className} ref={ref} onClick={activeCurrentLink}>
                 {label}
             </Link>
         ) )
